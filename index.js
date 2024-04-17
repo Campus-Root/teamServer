1e4b3f5b4038e08b65c54622406e2dd079c11187
viz-nu <vishnu.teja101.vt@gmail.com>
2024-04-17 19:56:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express()
+const port = 1235
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
