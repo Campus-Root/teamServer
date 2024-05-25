51fb1a9f885429a13b9cfef5de26b49a0126c2a6
viz-nu <vishnu.teja101.vt@gmail.com>
2024-05-25 13:22:56 +0530

    fix: removed x-powered-by header

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+app.disable('x-powered-by');
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
